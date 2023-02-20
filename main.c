8e24b317299e488460b2d0252478a98d99d42ac9
kiran5108 <kirankpatil93@gmail.com>
2023-02-20 11:07:30 +0000

    All files committed

---
+++ main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main() {
+         printf("\nBiggest of 2 numbers\n");
+         biggest2();
+	 printf("\nFactorial of a number\n");
+         factorial();
+	 printf("\nReverse\n");
+         reverse();
+	 printf("\nFibonacci\n");
+	 fibonacci();
+	 printf("\nSum of 2 numbers\n");
+	 adding();
+	 printf("\nSorting\n");
+	 sorting();
+	 printf("\nBiggest of 3 numbers\n");
+	 biggest3();
+	 printf("\nPalindrome of a number\n");
+	 palindrome();
+
+
+}
